eb5539125792d637f590907339fd1a843daa1bd4
sachin-dashzer <sachin8287037611@gmail.com>
2024-08-12 11:49:19 +0530

    express updated

--- backend/index.js
+++ backend/index.js
@@ -30,20 +30,48 @@
 //   console.log(`${data}`);
 // });
 
-
 // 4. http modules
 
+// const http = require('http');
 
-    // const http = require('http');
+// const server =  http.createServer( (req , res)=>{      // The server is created here
+//     res.statusCode = 200;
 
-    // const server =  http.createServer( (req , res)=>{      // The server is created here
-    //     res.statusCode = 200;
+//     res.end("<h1>hello brother</h1>");
 
-    //     res.end("<h1>hello brother</h1>");
-    
-    // })
+// })
 
-    // server.listen(3000 , ()=>{       // The server is started here on 3000 port number
+// server.listen(3000 , ()=>{       // The server is started here on 3000 port number
 
-    //     console.log("server is started");
-    // })
+//     console.log("server is started");
+// })
+
+/////////////////////////////////////// Express started from here //////////////////////////////////////////////////////////////////
+
+const express = require("express");
+const app = express();
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000;  // process.env file is used to extract the value of any variable from .env file
+
+                                    
+app.use((req, res, next) => {   // Define a middleware function that will be executed for every request
+
+    console.log("Middleware is working");
+
+    next();       // Call next() to pass control to the next middleware or route handler
+});
+
+
+
+app.get("/", (req, res) => {    // Define a route handler for the root URL ("/")
+  
+    res.send("<h1>Everything's Fine now</h1>");     // This sends a simple HTML response to the browser
+});
+
+
+app.listen(PORT, () => {        // Start the server and listen for requests on port 3000
+  
+  console.log(`Server Started on port ${PORT}`);
+});
