64f35263912eeab1276cd7eaed9fd9d3b6cf06cd
sachin-dashzer <sachin8287037611@gmail.com>
2024-08-26 12:47:51 +0530

    frontend changes

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,34 @@
+
+
+const Header = ()=>{
+
+    return(
+    <>
+    
+        <header>
+            <div className="topbar">
+                
+            </div>
+
+            <div className="navbar">
+                <div className="logoBox">
+                    <h2>Backend</h2>
+                </div>
+                <div className="nav-menu">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Blogs</a></li>
+                        <li><a href="#">Reviews</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+
+    </>
+
+)}
+
+
+export default Header
