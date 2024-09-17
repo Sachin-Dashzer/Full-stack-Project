6df0efadd5d45bb0b899a01a50da3106fad639aa
sachin-dashzer <sachin8287037611@gmail.com>
2024-09-17 17:49:46 +0530

    new frontend starts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,20 @@
+
+
+import React from 'react'
 import './App.css'
-import Header from './components/Header.jsx'
-import { Routes , Route } from 'react-router-dom';
 
-import Thanks from './components/Thanks.jsx';
-
-
-
-function App() {
-
+const App = () => {
   return (
+
     <>
-
-
-
-      <Header />
-      <Routes>
-
-          <Route path='/' element={<h1>Home</h1>} />
-          <Route path='/thanks' element={<Thanks />} />
-          <Route path='/contact' element={<h1>Contact</h1>} />
-          <Route path='/login' element={<h1>Login</h1>} />
-          <Route path='/signup' element={<h1>Signup</h1>} />
-
-      </Routes>
-     
+    
+    
+    
+    
+    
     </>
+
+
   )
 }
 
